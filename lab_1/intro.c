983a8277eae770e5d6fb1dd4272ff406744ca33d
dhritish007-ai <dhritishc@gmail.com>
2025-10-25 18:59:48 +0530

    Add files via upload

---
+++ lab_1/intro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Dhritish Choudhury.\n");
+    printf("My branch is Mechanical Engineering.\n");
+    printf("My roll no is MEB25010.\n");
+
+return 0;
+}
